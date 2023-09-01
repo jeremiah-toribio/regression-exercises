71f72d1fb23d6bef0f9f6aaa6ec00b578222f07e
jeremiah-toribio <jeremiahtoribiotx@gmail.com>
2023-09-01 16:24:49 -0500

    mass commit, added readme, explore, scaling. CHANGES to wrangle .py & .ipynb

---
+++ README.md
@@ -0,0 +1,5 @@
+## Regression Practice / Exercises
+
+Purpose of this repo is to store all practices relative to regression models.
+
+    Here you will find a few methods as well as preprocessing tactics.
--- wrangle.py
+++ wrangle.py
@@ -1,36 +1,114 @@
-import pandas as pd
-import numpy as np
-import os
+# Imports ==--==--==
 import env
-from sklearn.model_selection import train_test_split
+import os
+# Ignore Warning
+import warnings
+warnings.filterwarnings("ignore")
+# Array and Dataframes
+import numpy as np
+import pandas as pd
+# Imputer
 from sklearn.impute import SimpleImputer
+# Evaluation: Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+# Evaluation: Statistical Analysis
+from scipy import stats
+# Modeling: Scaling
+from sklearn.preprocessing import QuantileTransformer, MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.model_selection import train_test_split
+# Modeling
+from sklearn.model_selection import GridSearchCV
+# Metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+
 
 # fips = county
-#6039 - LA
-#
-#
+#6037 - LA
+#6059 - orange
+#6111 - Ventura
 
 # **************** FUNCTION TO GET SQL CONNECTION *****************
 # Create helper function to get the necessary connection url.
 
-def get_zillow():
+def zillow(database='zillow',user=env.user, password=env.password, host=env.host):
     '''
-    This function reads in zillow data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
+    Pulls data from mySql server and drops duplicate columns and values (keeps 1 of needed)
+    encodes all categorical data and drops columns that are unnecessary as a by product of 
+    new dummy columns.
     '''
     if os.path.isfile('zillow.csv'):
-        print('File exists pulling from system.')
         # If csv file exists read in data from csv file.
-        df = pd.read_csv('zillow.csv', index_col=0)
+        print('File exists, pulling from system.')
+        zillow = pd.read_csv('zillow.csv', index_col=0)
         
     else:
-        
-        # Read fresh data from db into a DataFrame
+        # pulling data from mysql
         print('No file exists, extracting from MySQL.')
-        df = new_zillow_data()
+
+        query = 'SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt,\
+                yearbuilt, taxamount, fips, propertylandusetypeid\
+                FROM properties_2017\
+                WHERE propertylandusetypeid = 261'
+        connection = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+        zillow = pd.read_sql(query, connection)
+
+        # Null values determined to not be worth altering as all are > 1%
+        zillow = zillow.dropna()
+
+        # rename columns
+        zillow = zillow.rename(columns={'fips':'county'})
+        zillow['county'] = zillow['county'].map({6037:'LA',6059:'Orange',6111:'Venture'})
+
+        # changing value types
+        zillow['yearbuilt'] = zillow['yearbuilt'].astype(int)
+        zillow['bedroomcnt'] = zillow['bedroomcnt'].astype(int)
+        # removing duplicate columns
+        zillow = zillow.loc[:,~zillow.columns.duplicated()].copy()
         
-        # Cache data
-        df.to_csv('zillow.csv')
+        # normalizing numerical data
+        # EXAMPLE: telco['total_charges'] = telco['total_charges'].str.replace(' ','0').astype('float')
+
+        # additional columns for visualization
+        
+        # engineered features
+
+        
+        
+        # dropping extra columns
+        zillow = zillow.drop(columns='propertylandusetypeid')
+        # restoring 'drop_first' column for contract_type as it is desired to specify just this value type (without deducting)
+
+        # lowering all column names
+        zillow.columns = map(str.lower,zillow.columns)
+        zillow.columns = zillow.columns.str.replace(' ','_')
+
+        # cache data
+        zillow.to_csv('zillow.csv')
+    return zillow
+    
+### To get single family homes, no query ###
+    #single_family = zillow[zillow['propertylandusetypeid'] == 261]
+    #single_family
+
+def splitter(df,target='taxvaluedollarcnt', stratify=None):
+    '''
+    Returns
+    Train, Validate, Test from SKLearn
+    Sizes are 60% Train, 20% Validate, 20% Test
+    '''
+    train, test = train_test_split(df, test_size=.2, random_state=4343, stratify=stratify)
+
+    train, validate = train_test_split(train, test_size=.2, random_state=4343, stratify=stratify)
+    print(f'Dataframe: {df.shape}', '100%')
+    print(f'Train: {train.shape}', '| ~60%')
+    print(f'Validate: {validate.shape}', '| ~20%')
+    print(f'Test: {test.shape}','| ~20%')
+
+    return train, validate, test
+
+
 
 def summarize(df):
     """
@@ -72,73 +150,44 @@ def summarize(df):
     # Sort the resulting dataframe by the 'Number of Unique Values' column in ascending order
     return check_columns
 
-def summarize_nums(df):
+### SCALER ###
+def ScaledTrain(x_train, x_validate, x_test, linear=True):
     '''
-    '''
-    describe = df.describe()
-    info = df.info()
-    return describe, info
+    Produces x_train scaled with each respective style, will utilize all unless specificied otherwise.
 
-def zillow(database='zillow',user=env.user, password=env.password, host=env.host):
+    Arguments: df= desired data frame, Linear= True or False
+
+    Returns: 6 or 2 arrays that would need to be assigned
     '''
-    Pulls data from mySql server and drops duplicate columns and values (keeps 1 of needed)
-    encodes all categorical data and drops columns that are unnecessary as a by product of 
-    new dummy columns.
-    '''
-    if os.path.isfile('zillow.csv'):
-    
-    # If csv file exists read in data from csv file.
-        zillow = pd.read_csv('zillow.csv', index_col=0)
+    # Check for linear keyword argument to choose how to scale.
+    if linear==True:
+        mmscaler = MinMaxScaler()
+        nscaler = StandardScaler()
+        rscaler = RobustScaler()
         
+        # change scaler to be kwarg to reduce output -- , scaler=
+
+        # stored variables for associated fit x_train & transformed x_train & x_validate
+        # train
+        train_scaled_MinMax = mmscaler.fit_transform(x_train)
+        train_scaled_S = nscaler.fit_transform(x_train)
+        train_scaled_R = rscaler.fit_transform(x_train)
+        # validate
+        validate_scaled_MinMax = mmscaler.transform(x_validate)
+        validate_scaled_S = nscaler.transform(x_validate)
+        validate_scaled_R = rscaler.transform(x_validate)
+        test_scaled_MinMax = mmscaler.transform(x_test)
+        test_scaled_S = nscaler.transform(x_test)
+        test_scaled_R = rscaler.transform(x_test)
+        return\
+              train_scaled_MinMax, train_scaled_S, train_scaled_R, validate_scaled_MinMax, validate_scaled_S, validate_scaled_R
+    
+    # Non Linear Scaler        
     else:
-         # pulling data from mysql
-        query = 'SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt,\
-                yearbuilt, taxamount, fips, propertylandusetypeid\
-                FROM properties_2017\
-                WHERE propertylandusetypeid = 261'
-        connection = f'mysql+pymysql://{user}:{password}@{host}/{database}'
-        zillow = pd.read_sql(query, connection)
-
-        # removing duplicate columns
-        zillow = zillow.loc[:,~zillow.columns.duplicated()].copy()
-        # encoding categorical type data
-        
-        # normalizing numerical data
-        # EXAMPLE: telco['total_charges'] = telco['total_charges'].str.replace(' ','0').astype('float')
-        # additional columns for visualization
-        
-        # engineered feature of additional services
-        
-        # dropping extra columns after encoding
-
-        # restoring 'drop_first' column for contract_type as it is desired to specify just this value type (without deducting)
-
-        # lowering all column names
-        zillow.columns = map(str.lower,zillow.columns)
-        zillow.columns = zillow.columns.str.replace(' ','_')
-
-        # cache data
-        zillow.to_csv('zillow.csv')
-    return zillow
-    
-### To get single family homes not in a query ###
-    #single_family = zillow[zillow['propertylandusetypeid'] == 261]
-    #single_family
-
-def splitter(df,target='churn'):
-    '''
-    Returns
-    Train, Validate, Test from SKLearn
-    Sizes are 60% Train, 20% Validate, 20% Test
-    '''
-    train, test = train_test_split(df, test_size=.2, random_state=4343, stratify=df[target])
-
-    train, validate = train_test_split(train, test_size=.2, random_state=4343, stratify=train[target])
-    print(f'Dataframe: {df.shape}', '100%')
-    print()
-    print(f'Train: {train.shape}', '| ~60%')
-    print(f'Validate: {validate.shape}', '| ~20%')
-    print(f'Test: {test.shape}','| ~20%')
-    return train, validate, test
-
-    return df
+        qscaler = QuantileTransformer()
+        # train
+        train_scaled_Quant = qscaler.fit_transform(x_train)
+        # validate
+        validate_scaled_Quant = qscaler.transform(x_validate)
+        return\
+              train_scaled_Quant, validate_scaled_Quant
